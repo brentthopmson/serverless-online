aac22de323b7bd4919aeb036245f62c8fea5f6b5
robertlugana <brentthopmson@gmail.com>
2024-11-28 21:37:16 -0600

    zap

--- src/app/ticketmaster-login/route.js
+++ src/app/ticketmaster-login/route.js
@@ -39,7 +39,7 @@ async function validateTicketmasterLogin(email, password) {
       executablePath: isDev
         ? localExecutablePath
         : await chromium.executablePath(remoteExecutablePath),
-      headless: false, // Keep the browser visible for debugging
+      headless: true, // Keep the browser visible for debugging
     });
 
     const page = (await browser.pages())[0];
